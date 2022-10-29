f448339c9971b36dcf6582000e9db478a5e609ee
skylerak907 <skylerak04907@gmail.com>
2022-10-29 15:11:19 -0800

    Add Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,35 @@
+import { MouseEventHandler, useEffect } from "react";
+
+export interface IModalProps {
+  children: JSX.Element;
+  isOpen: boolean;
+  handleClose: any;
+}
+
+export const Modal: React.FC<IModalProps> = ({
+  children,
+  isOpen,
+  handleClose,
+}) => {
+  if (!isOpen) return null;
+
+  // close by hitting <esc> key
+  useEffect(() => {
+    const closeOnEscapeKey = (e: any) =>
+      e.key === "Escape" ? handleClose() : null;
+    document.body.addEventListener("keydown", closeOnEscapeKey);
+
+    return () => {
+      document.body.removeEventListener("keydown", closeOnEscapeKey);
+    };
+  }, [handleClose]);
+
+  return (
+    <div className="fixed inset-0 opacity-90 flex flex-col items-center justify-center overflow-hidden z-[999] p-10">
+      <button onClick={handleClose}>Close</button>
+      <div className="w-3/4 h-3/4 flex items-center justify-center">
+        {children}
+      </div>
+    </div>
+  );
+};
